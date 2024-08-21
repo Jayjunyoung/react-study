072d4a42093cc86c6aa9a04b36ce963d28a5efd6
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-21 20:30:02 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("junyoung2");
+
+  function handleClick() {
+    setName("junyoung");
+    console.log(name); // 아직 "junyoung2"
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+
+        <img
+          src={reactLogo}
+          className="logo react"
+          alt="React logo"
+          onClick={handleClick}
+        />
+      </div>
+      <h1>Vite + React + {name}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
