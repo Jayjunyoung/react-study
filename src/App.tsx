a99b8460144a48328509efda47eab5558b58c662
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-29 13:48:05 +0900

    리액트 공식문서 useRef 챕터 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -6,6 +6,19 @@ import viteLogo from "/vite.svg";
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("junyoung2");
+
+  let ref = useRef(0); //current객체 반환
+  let inputRef = useRef<HTMLInputElement>(null);
+
+  function inputClick() {
+    inputRef.current?.focus();
+  }
+
+  //이벤트 헨들러나 useEffect에서 ref를 읽을 수 있음, 아니면 사용 x
+  function handleRefClick() {
+    ref.current = ref.current + 1; //ref로 렌더링에 관여는 안하게하면서 current객체 값 증가 시키기
+    alert("You clicked " + ref.current + " times!");
+  }
 
   function handleClick() {
     setName("junyoung");
@@ -31,10 +44,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p onClick={handleRefClick}>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <input ref={inputRef} />
+      <button onClick={inputClick}>input Ref 실습</button>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
